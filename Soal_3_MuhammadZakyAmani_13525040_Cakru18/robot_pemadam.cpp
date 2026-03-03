e01d06ff8398fd2daeccfee9b3df08d5d3c3ea70
Zaky Amani <mzakyrice@gmail.com>
2026-03-03 20:59:08 +0700

    upload soal 2 dan 3

--- Soal_3_MuhammadZakyAmani_13525040_Cakru18/robot_pemadam.cpp
+++ Soal_3_MuhammadZakyAmani_13525040_Cakru18/robot_pemadam.cpp
@@ -1,5 +1,50 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
+class RobotPemadam{
+private:
+    int jarak;
+    string status;
+
+public:
+    void inputSensor(int input){
+        jarak = input;
+    }
+
+    void prosesLogika(){
+        if(jarak > 20){
+            status = "Maju mencari Api";
+        }else if(jarak > 5){
+            status = "UDAH DEKET NIH BRAY";
+        }else{
+            status = "Posisi Tepat! gas semprot kali ya!";
+        }
+    }
+
+    void cetakStatus(){
+        cout << "[Sensor: " << jarak << " cm] -> Action: [" << status <<"]" << endl;
+    }
+};
+
 int main(){
-    
+    RobotPemadam robot;
+    int input_jarak;
+
+    while(true){
+        cout << "Masukkan jarak sensor: ";
+        cin >> input_jarak;
+
+        if(input_jarak == 67){
+            cout << "Program Berhenti." << endl;
+            break;
+        }
+
+        robot.inputSensor(input_jarak);
+        robot.prosesLogika();
+        robot.cetakStatus();
+    }
+
+    return 0;
+
 }
